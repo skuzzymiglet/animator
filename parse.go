546b9886153b9fdf0c8a7167c800788680637d15
skuzzymiglet <skuzzymiglet@gmail.com>
2019-12-15 08:40:11 +0000

    parse.go comments

--- parse.go
+++ parse.go
@@ -8,18 +8,21 @@ import (
 )
 
 type rule struct {
-	re     string
-	expand func([]byte) []byte
+	// Replacement Rule
+	re     string              // What to search for
+	expand func([]byte) []byte // What to do on it (replaceAllFunc)
 }
 
 func expandRange(inText []byte) []byte {
+	// Expand range e.g. 4-7, into comma separated e.g 4,5,6,7
 	s := ""
 	text := string(inText)
-	start, serr := strconv.Atoi(strings.Split(text, "-")[0])
-	end, eerr := strconv.Atoi(strings.Split(text, "-")[1])
+	start, serr := strconv.Atoi(strings.Split(text, "-")[0]) // 4-7 -> 4
+	end, eerr := strconv.Atoi(strings.Split(text, "-")[1])   // 4-7 -> 7
 	if serr != nil || eerr != nil {
 		fmt.Printf("err: %q\n", text)
 	}
+	// RANGE
 	counter := 0
 	if end-start >= 0 {
 		counter = 1
@@ -27,8 +30,9 @@ func expandRange(inText []byte) []byte {
 		counter = -1
 	}
 	for i := start; i != end+counter; i += counter {
+		// TODO factor
 		if i == end {
-			s += strconv.Itoa(i)
+			s += strconv.Itoa(i) // no comma at end
 		} else {
 			s += strconv.Itoa(i) + ","
 		}
@@ -37,14 +41,19 @@ func expandRange(inText []byte) []byte {
 }
 
 func expandRepeat(inText []byte) []byte {
+	// Expand repeat e.g 4(3), into comma separated e.g 3,3,3,3
 	s := ""
 	text := string(inText)
-	timesString := regexp.MustCompile("(\\d+)\\(.+\\)").FindStringSubmatch(text)[1]
+	timesString := regexp.MustCompile("(\\d+)\\(.+\\)").FindStringSubmatch(text)[1] // 6(7-90) -> 6
 	times, _ := strconv.Atoi(timesString)
+	// do not do 0 times (repeat till end)
+	// TODO expandRepeatTillEnd
 	if times == 0 {
 		return inText
 	}
-	inside := regexp.MustCompile("\\d+\\((.+)\\)").FindStringSubmatch(text)[1]
+
+	inside := regexp.MustCompile("\\d+\\((.+)\\)").FindStringSubmatch(text)[1] // 6(7-90) -> 7-90
+	// append inside to returning string `times` times
 	for i := 0; i != times; i += 1 {
 		s += inside
 		if i != times-1 {
@@ -55,6 +64,7 @@ func expandRepeat(inText []byte) []byte {
 }
 
 func ReplaceAll(s string) string {
+	// Replace (expand?) all according to a list of `rule`s
 	rules := []rule{rule{"[0-9]+\\-[0-9]+", expandRange}, rule{"\\d+\\([^\\)]+\\)", expandRepeat}}
 	for r := range rules {
 		s = string(regexp.MustCompile(rules[r].re).ReplaceAllFunc([]byte(s), rules[r].expand))
